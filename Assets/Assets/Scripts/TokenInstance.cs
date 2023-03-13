8afd25d23f8c43ddda96a7ddb63c49535019f05f
JBonda <jbonda@my.centennialcollege.ca>
2023-03-13 12:38:57 -0600

    my complete project 20230313

---
+++ Assets/Assets/Scripts/TokenInstance.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TokenInstance : MonoBehaviour
+{
+    /*
+    private void OnPlayerConnected(NetworkPlayer player)
+    {
+        
+    }
+    */
+
+}
